7fdc853c6a59dea0b97fe7ee172b8f06a0540f53
lovakumart <48078694+lovakumart@users.noreply.github.com>
2025-12-16 11:08:14 -0500

    Add files via upload

--- poc-script.js
+++ poc-script.js
@@ -61,9 +61,7 @@ function sendWeatherDataToAEP() {
           offerDiv.innerHTML = "";
 
           if (!allOffers.length) {
-            offerDiv.innerHTML = "<h2>Protect Your Skin This Summer</h2>   
-	<p>High temperatures mean high UV risk. Get <strong>20% off</strong> our dermatologist-recommended sunscreens and skin protection kits.</p>   
-	<p>Offer valid this week only for areas with temperatures over 90°F.</p> <button> Shop Sunscreen</button>";
+            offerDiv.innerHTML = "<h2>Protect Your Skin This Summer</h2><p>High temperatures mean high UV risk. Get <strong>20% off</strong> our dermatologist-recommended sunscreens and skin protection kits.</p><p>Offer valid this week only for areas with temperatures over 90°F.</p>";
             return;
           }
 
